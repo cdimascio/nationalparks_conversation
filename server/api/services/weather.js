29b4d56672a8b2258eb69dc84a5b728018a3ed9e
Carmine DiMascio <dimascio@us.ibm.com>
2017-01-21 14:02:35 -0500

    update weather api call

--- server/api/services/weather.js
+++ server/api/services/weather.js
@@ -2,7 +2,7 @@ import {RxHttpRequest} from 'rx-http-request';
 
 export class Weather {
   byGeo(lat,lon) {
-    const uri = `https://${process.env.WEATHER_USERNAME}:${process.env.WEATHER_PASSWORD}@twcservice.mybluemix.net:443/api/weather/v1/geocode/${lat}/${lon}/observations.json`;
+    const uri = `${process.env.WEATHER_API_ROOT}/api/weather/v1/geocode/${lat}/${lon}/observations.json`;
     return RxHttpRequest.get(uri, {
       qs: {
         units: 'e',
